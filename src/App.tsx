85796836c4165df89228bd64145117be06b3105a
Christian Diaz Portela <christiandp7@gmail.com>
2021-10-07 10:58:14 -0400

    :lipstick: Add YT and Mail to mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { ComingSoonText } from '@components/svg'
 import XboxController from '@components/XboxController'
 import { FaFacebookF } from 'react-icons/fa'
 import { FaWhatsapp } from 'react-icons/fa'
+import { FaYoutube } from 'react-icons/fa'
+import { BiEnvelope } from 'react-icons/bi'
 import { whatsAppLink } from '@utils/whatsappLink'
 // assets
 import logo from '@images/logo-face.png'
@@ -37,6 +39,21 @@ function App() {
 								target="_blank"
 								href={whatsAppLink()}>
 								<FaWhatsapp />
+							</a>
+						</li>
+						<li className={s.socialItem}>
+							<a
+								className={cn(s.socialButton, s.yt)}
+								target="_blank"
+								href="https://www.youtube.com/channel/UCcU27T_dt7JWy-uB7BO_fLQ">
+								<FaYoutube />
+							</a>
+						</li>
+						<li className={s.socialItem}>
+							<a
+								href="mailto:evercin.15@gmail.com"
+								className={cn(s.socialButton, s.mail)}>
+								<BiEnvelope />
 							</a>
 						</li>
 					</ul>
